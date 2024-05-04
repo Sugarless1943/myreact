816e2aaf3d65c325c3afe71f54891b4cece62f20
Fan.Ye <fan.ye@yo-i.net>
2024-05-04 18:45:32 +0800

    2

--- src/MyReact/render.js
+++ src/MyReact/render.js
@@ -1,4 +1,54 @@
-function render(element, container) {
+let nextUnitWork = null;
+
+function perforUnitOfWork(fiber) {
+  if (!fiber.dom) fiber.dom = createDom(fiber);
+
+  if (fiber.parent) fiber.parent.dom.append(fiber.dom);
+
+  const elements = fiber.props.children;
+
+  let prevFiber = null;
+  elements.forEach((element, index) => {
+    const newFiber = {
+      parent: fiber,
+      dom: null,
+      props: element.props,
+      type: element.type,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevFiber.sibling = newFiber;
+    }
+
+    prevFiber = newFiber;
+  });
+
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+
+    nextFiber = nextFiber.parent;
+  }
+}
+
+function workloop(deadline) {
+  if (nextUnitWork && deadline.timeRemaining() > 1) {
+    nextUnitWork = perforUnitOfWork(nextUnitWork);
+  }
+
+  requestIdleCallback(workloop);
+}
+requestIdleCallback(workloop);
+
+function createDom(element) {
   const dom =
     element.type === "TEXT_ELEMENT"
       ? document.createTextNode(element.props.nodeValue)
@@ -13,9 +63,16 @@ function render(element, container) {
       });
   }
 
-  element?.props?.children.forEach((item) => render(item, dom));
+  return dom;
+}
 
-  container.appendChild(dom);
+function render(element, container) {
+  nextUnitWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
 }
 
 export default render;
